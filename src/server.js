ae785070e38a4e43b0ad4e4434333fb2a77ed285
fancyers <kjng5901@gmail.com>
2022-02-11 17:50:25 +0900

    채팅 기본 기능 구현

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from 'http'
+import WebSocket from 'ws'
 import express from 'express'
 
 const app = express()
@@ -9,4 +11,31 @@ app.get('/', (_, res) => res.render('home'))
 app.get('/*', (_, res) => res.redirect('/')) //  다른 주소로 접속시 redirect
 
 const handleListen = () => console.log('Listening on http://localhost:3000')
-app.listen(3000, handleListen)
+
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server }) //  http 서버 위에 ws 서버를 만듦
+
+const sockets = []
+
+wss.on('connection', (socket) => {
+    sockets.push(socket)
+    socket['nickname'] = 'Anon'
+    console.log('Connected to Browser ✅')
+    socket.on('close', () => console.log('Disconnected from Browser ❌'))
+    socket.on('message', (msg) => {
+        const messageString = msg.toString('utf-8')
+        const message = JSON.parse(messageString)
+        switch (message.type) {
+            case 'new_message':
+                sockets.forEach((aSocket) =>
+                    aSocket.send(`${socket.nickname}: ${message.payload}`)
+                )
+                break
+            case 'nickname':
+                socket['nickname'] = message.payload
+                break
+        }
+    })
+})
+
+server.listen(3000, handleListen)
