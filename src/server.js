3473cb3b3700c8233f3e3d5dbe2cb82ec6de3185
fancyers <kjng5901@gmail.com>
2022-02-27 14:09:40 +0900

    줌 클론코딩 ~ 2.6

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from 'http'
-import WebSocket from 'ws'
+// import WebSocket from 'ws'
+import SocketIO from 'socket.io'
 import express from 'express'
 
 const app = express()
@@ -12,7 +13,28 @@ app.get('/*', (_, res) => res.redirect('/')) //  다른 주소로 접속시 redi
 
 const handleListen = () => console.log('Listening on http://localhost:3000')
 
-const server = http.createServer(app)
+const httpServer = http.createServer(app)
+const wsServer = SocketIO(httpServer)
+
+wsServer.on('connection', (socket) => {
+	socket.onAny((event) => {
+		console.log(`Socket Event: ${event}`)
+	})
+	socket.on('enter_room', (roomName, done) => {
+		socket.join(roomName)
+		done()
+		socket.to(roomName).emit('welcome') //  이 방의 나를 제외한 사람들에게 메시지 보내기
+	})
+	socket.on('disconnecting', () => {
+		socket.rooms.forEach((room) => socket.to(room).emit('bye'))
+	})
+	socket.on('new_message', (msg, room, done) => {
+        socket.to(room).emit('new_message', msg)
+        done()
+    })
+})
+
+/* 
 const wss = new WebSocket.Server({ server }) //  http 서버 위에 ws 서버를 만듦
 
 const sockets = []
@@ -37,5 +59,5 @@ wss.on('connection', (socket) => {
         }
     })
 })
-
-server.listen(3000, handleListen)
+ */
+httpServer.listen(3000, handleListen)
