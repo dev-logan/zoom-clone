8f1817767a482b2562a05d96567389b5a818792a
fancyers <kjng5901@gmail.com>
2022-02-28 21:09:50 +0900

    줌 클론코딩 ~ 3.7

--- src/server.js
+++ src/server.js
@@ -12,8 +12,6 @@ app.use('/public', express.static(__dirname + '/public'))
 app.get('/', (_, res) => res.render('home'))
 app.get('/*', (_, res) => res.redirect('/')) //  다른 주소로 접속시 redirect
 
-const handleListen = () => console.log('Listening on http://localhost:3000')
-
 const httpServer = http.createServer(app)
 const wsServer = new Server(httpServer, {
 	cors: {
@@ -25,77 +23,21 @@ instrument(wsServer, {
 	auth: false,
 })
 
-function publicRooms() {
-	const {
-		sockets: {
-			adapter: { sids, rooms },
-		},
-	} = wsServer
-	const publicRooms = []
-	rooms.forEach((_, key) => {
-		if (sids.get(key) === undefined) {
-			publicRooms.push(key)
-		}
-	})
-	return publicRooms
-}
-
-function countRoom(roomName) {
-	return wsServer.sockets.adapter.rooms.get(roomName)?.size
-}
-
 wsServer.on('connection', (socket) => {
-	socket['nickname'] = 'Anon'
-	socket.onAny((event) => {
-		console.log(`Socket Event: ${event}`)
-	})
-	socket.on('enter_room', (roomName, done) => {
+	socket.on('join_room', (roomName) => {
 		socket.join(roomName)
-		done(countRoom(roomName))
-		socket
-			.to(roomName)
-			.emit('welcome', socket.nickname, countRoom(roomName)) //  이 방의 나를 제외한 사람들에게 메시지 보내기
-		wsServer.sockets.emit('room_change', publicRooms())
+		socket.to(roomName).emit('welcome')
 	})
-	socket.on('disconnecting', () => {
-		socket.rooms.forEach((room) =>
-			socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1)
-		)
+	socket.on('offer', (offer, roomName) => {
+		socket.to(roomName).emit('offer', offer)
 	})
-	socket.on('disconnect', () => {
-		wsServer.sockets.emit('room_change', publicRooms())
+	socket.on('answer', (answer, roomName) => {
+		socket.to(roomName).emit('answer', answer)
 	})
-	socket.on('new_message', (msg, room, done) => {
-		socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`)
-		done()
+	socket.on('ice', (ice, roomName) => {
+		socket.to(roomName).emit('ice', ice)
 	})
-	socket.on('nickname', (nickname) => (socket['nickname'] = nickname))
 })
 
-/* 
-const wss = new WebSocket.Server({ server }) //  http 서버 위에 ws 서버를 만듦
-
-const sockets = []
-
-wss.on('connection', (socket) => {
-    sockets.push(socket)
-    socket['nickname'] = 'Anon'
-    console.log('Connected to Browser ✅')
-    socket.on('close', () => console.log('Disconnected from Browser ❌'))
-    socket.on('message', (msg) => {
-        const messageString = msg.toString('utf-8')
-        const message = JSON.parse(messageString)
-        switch (message.type) {
-            case 'new_message':
-                sockets.forEach((aSocket) =>
-                    aSocket.send(`${socket.nickname}: ${message.payload}`)
-                )
-                break
-            case 'nickname':
-                socket['nickname'] = message.payload
-                break
-        }
-    })
-})
- */
+const handleListen = () => console.log('Listening on http://localhost:3000')
 httpServer.listen(3000, handleListen)
